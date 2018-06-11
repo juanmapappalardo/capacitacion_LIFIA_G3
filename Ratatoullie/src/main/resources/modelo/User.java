36992cb8dbefb217dff28c993c8075a48e2daad8
JuanMa <juanma.pappalardo@gmail.com>
2018-06-10 23:01:39 -0300

    AppRoot, User

---
+++ Ratatoullie/src/main/resources/modelo/User.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class User {
+
+	private String nombre;
+	private String apellido; 
+	private String telefono; 
+	private String password; 
+	private String email; 
+	
+}
+ 	
